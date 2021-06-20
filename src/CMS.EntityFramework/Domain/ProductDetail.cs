2844e24aa354ddbd4d6fcd9445f3729ff2156981
NguyenVanDu2001 <nguyenduc1908i1bkap@gmail.com>
2021-06-20 18:25:38 +0700

    Create entity ProductDetail

---
+++ src/CMS.EntityFramework/Domain/ProductDetail.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace CMS.EntityFramework.Domain
+{
+    public class ProductDetail : BaseEntity
+    {
+        public string  Name{ get; set; }
+        public string Slug { get; set; }
+        public string Description { get; set; }
+        public string UrlImage { get; set; }
+        public int Quantity { get; set; }
+        public Guid  ProductId{ get; set; }
+        [ForeignKey("ProductId")]
+        public virtual Product Products { get; set; }
+
+    }
+}
